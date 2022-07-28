a0ec4f46ba4843955cdc00b96200071898ca6576
munjehun <miso1489@gmail.com>
2022-07-28 14:38:48 +0900

    작가 오프라인 계약 API 수정3, 계약내용 컴포넌트화3

--- src/pages/OfflineContract_Painter.js
+++ src/pages/OfflineContract_Painter.js
@@ -11,7 +11,7 @@ function OfflineContract() {
   const [buyer_name, setBuyer_name] = useState("");
   const [owner_name, setOwner_name] = useState("");
   const [art_price, setArt_price] = useState("");
-  const user_artistname = JSON.parse(sessionStorage.getItem("user_artistname"));
+  const [art_owner, setArt_owner] = useState("");
 
   useEffect(() => {
     TradeDetail();
@@ -38,6 +38,7 @@ function OfflineContract() {
         setBuyer_name(res.data.buyer_name);
         setOwner_name(res.data.owner_name);
         setArt_price(res.data.art_price);
+        setArt_owner(res.data.art_owner);
       })
       .catch((err) => {
         console.log(err);
@@ -51,16 +52,18 @@ function OfflineContract() {
     <>
       {trade_state == 2 ? (
         <OfflineContractWaiting id={id} trade_user_id={trade_user_id} />
-      ) : (
+      ) : trade_state == "3" ? (
         <OfflineContractCheck
-          user_artistname={user_artistname} // 작가명
           id={id} // 작품 id
           trade_user_id={trade_user_id} // 구매자 아이디
+          art_owner={art_owner} // 작가명
           owner_name={owner_name} // 작가 이름
           buyer_name={buyer_name} // 구매자 이름
           art_price={art_price} // 가격
           art_name={art_name} // 작품 이름
         />
+      ) : (
+        <div>계약 진행중이 아닙니다.</div>
       )}
     </>
   );
--- src/pages/OfflineContract_User.js
+++ src/pages/OfflineContract_User.js
@@ -12,7 +12,7 @@ function OfflineContractUser() {
   const [buyer_name, setBuyer_name] = useState("");
   const [owner_name, setOwner_name] = useState("");
   const [art_price, setArt_price] = useState("");
-  const user_artistname = JSON.parse(sessionStorage.getItem("user_artistname"));
+  const [art_owner, setArt_owner] = useState("");
 
   useEffect(() => {
     TradeDetail();
@@ -32,26 +32,28 @@ function OfflineContractUser() {
       })
       .then((res) => {
         console.log("오프라인 계약 API 수정 후 res.data: ", res.data);
-        console.log("오프라인 계약 API 수정 후 res: ", res);
         setTrade_state(res.data.trade_state);
         setArt_name(res.data.art_name);
         setBuyer_name(res.data.buyer_name);
         setOwner_name(res.data.owner_name);
         setArt_price(res.data.art_price);
+        setArt_owner(res.data.art_owner);
       })
       .catch((err) => {
         console.log(err);
       });
   };
 
+  console.log("유저 오프라인 계약 페이지에서의 trade_state : ", trade_state);
+
   // API불러와서 trade_state값이 2면 대기화면, 3이면 계약체크리스트 화면 출력
   return (
     <>
       {trade_state == 2 ? (
         <OfflineContractCheckUser
-          user_artistname={user_artistname} // 작가명
           id={id} // 작품 id
           trade_user_id={trade_user_id} // 구매자 아이디
+          art_owner={art_owner} // 작가명
           owner_name={owner_name} // 작가 이름
           buyer_name={buyer_name} // 구매자 이름
           art_price={art_price} // 가격
